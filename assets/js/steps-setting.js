9a83fd3ed1a2637813c53087a3682224ac059038
BiancaFSilva <biancaflorianodasilva@gmail.com>
2021-04-30 19:23:10 -0300

    Update header e páginas de login

---
+++ assets/js/steps-setting.js
@@ -0,0 +1,33 @@
+$(".tab-wizard").steps({
+	headerTag: "h5",
+	bodyTag: "section",
+	transitionEffect: "fade",
+	titleTemplate: '<span class="step">#index#</span> #title#',
+	labels: {
+		finish: "Cadastrar"
+	},
+	onStepChanged: function (event, currentIndex, priorIndex) {
+		$('.steps .current').prevAll().addClass('disabled');
+	},
+	onFinished: function (event, currentIndex) {
+		$('#success-modal').modal('show');
+	}
+});
+
+$(".tab-wizard2").steps({
+	headerTag: "h5",
+	bodyTag: "section",
+	transitionEffect: "fade",
+	titleTemplate: '<span class="step">#index#</span> <span class="info">#title#</span>',
+	labels: {
+		finish: "Cadastrar",
+		next: "Próximo",
+		previous: "Anterior",
+	},
+	onStepChanged: function(event, currentIndex, priorIndex) {
+		$('.steps .current').prevAll().addClass('disabled');
+	},
+	onFinished: function(event, currentIndex) {
+		$('#success-modal-btn').trigger('click');
+	}
+});
