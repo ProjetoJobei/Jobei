bdb1f4638a623aacc60dce3e42cf46eff13074f5
Bianca Silva <biancaflorianodasilva@gmail.com>
2021-05-05 08:22:11 -0300

    Update conecta.php

---
+++ restrict/conecta.php
@@ -0,0 +1,14 @@
+<?php
+    $servidor = "localhost";
+    $banco = ""; 
+    $usuario = "root";
+    $senha = "";
+
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+    if ($conexao -> connect_error) {
+        echo "Erro de conexão";
+    } else {
+        //echo "Conectado";
+    }
+?>
